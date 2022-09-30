9a5b926fbdbb798fecb4f960b5da609cced19f1a
Ray Consulting <rayconsulting@shopeneur.com>
2022-09-30 09:00:50 -0400

    added identity proofing portal

---
+++ src/RCL.Identity/RCL.Core.Identity.Proofing.Portal/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using RCL.Core.Identity.Proofing;
+using RCL.Core.Identity.Tools;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRCLCoreAuthTokenServices(options => builder.Configuration.Bind("Auth", options));
+builder.Services.AddRCLCoreIdentityGraphServices();
+builder.Services.AddRCLCoreIdentitySecurityGroupServices();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("UserAdmins", policy =>
+       policy.Requirements.Add(new GroupsCheckRequirement(new string[] { "UserAdmins" })));
+});
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddRCLCoreIdentityProofingServices(optionsApi => builder.Configuration.Bind("IdentityProofingApi", optionsApi));
+
+builder.Services.AddRazorPages()
+    .AddMicrosoftIdentityUI();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+
+app.Run();
